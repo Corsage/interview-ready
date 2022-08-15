d66d103767331c1747e2af7b129c6da802fcc47b
Jay Chowdhary <jayc.chowdhary@gmail.com>
2022-08-14 21:42:37 -0400

    Added bit manipulation soln.

--- leetcode/Single Number/solution.cpp
+++ leetcode/Single Number/solution.cpp
@@ -1,5 +1,18 @@
 class Solution {
 public:
+
+    /*
+     * we use bit manipulation via XOR. This works because the following.
+     * 1) n XOR n = 0
+     * 2) a XOR b = c and then c XOR a = b and then b XOR b = 0.
+     * 3) If c is not given, then that is the singular number.
+     * 
+     * Time complexity - O(n) and Space complexity - O(1).
+     */
+    int singleNumber(vector<int>& nums) {
+        return accumulate(nums.begin(), nums.end(), 0, bit_xor<int>());
+    }
+
     /*
      * We sort the array first because then we know the structure
      * will be either [Y, X, X] or [X, X, Y, Z, Z] or [Z, Z, Y].
